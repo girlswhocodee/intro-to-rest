30451ea4faff36ed0b752f9dee5fe3fd766f8297
girlswhocodee <myekini1@umbc.edu>
2020-08-06 18:51:18 -0400

    Beginning changes

---
+++ src/main/java/com/stayready/poll_application/domain/Option.java
@@ -0,0 +1,34 @@
+package com.stayready.poll_application.domain;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+public class Option {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "OPTION_ID")
+    private Long id;
+    @Column(name = "OPTION_VALUE")
+    private String value;
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public void setValue(String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return this.value;
+    }
+
+    
+}
---
+++ src/main/java/com/stayready/poll_application/domain/Poll.java
@@ -0,0 +1,6 @@
+package com.stayready.poll_application.domain;
+
+public class Poll {
+    
+    
+}
